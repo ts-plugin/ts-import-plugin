45d93fffefe641d522ad2c8b06062db1c89e7c5c
LongYinan <lynweklm@gmail.com>
2019-06-21 23:08:24 +0800

    feat: add resolveContext option
    close https://github.com/Brooooooklyn/ts-import-plugin/issues/89

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export interface Options {
   camel2DashComponentName?: boolean
   camel2UnderlineComponentName?: boolean
   transformToDefaultImport?: boolean
+  resolveContext?: string[]
 }
 
 export interface ImportedStruct {
@@ -86,8 +87,8 @@ function createDistAst(struct: ImportedStruct, options: Options) {
   const importName = options.camel2UnderlineComponentName
     ? camel2Underline(_importName)
     : options.camel2DashComponentName
-      ? camel2Dash(_importName)
-      : _importName
+    ? camel2Dash(_importName)
+    : _importName
 
   const libraryDirectory =
     typeof options.libraryDirectory === 'function'
@@ -103,7 +104,9 @@ function createDistAst(struct: ImportedStruct, options: Options) {
 
   const importPath = join(libraryName!, libraryDirectory)
   try {
-    require.resolve(importPath)
+    require.resolve(importPath, {
+      paths: [process.cwd(), ...options.resolveContext!],
+    })
     const scriptNode = ts.createImportDeclaration(
       undefined,
       undefined,
@@ -119,8 +122,8 @@ function createDistAst(struct: ImportedStruct, options: Options) {
               ),
             ])
           : options.transformToDefaultImport
-            ? undefined
-            : ts.createNamedImports([ts.createImportSpecifier(undefined, ts.createIdentifier(struct.importName))]),
+          ? undefined
+          : ts.createNamedImports([ts.createImportSpecifier(undefined, ts.createIdentifier(struct.importName))]),
       ),
       ts.createLiteral(importPath),
     )
@@ -160,6 +163,7 @@ const defaultOptions = {
   style: false,
   camel2DashComponentName: true,
   transformToDefaultImport: true,
+  resolveContext: [],
 }
 
 export function createTransformer(_options: Partial<Options> | Array<Partial<Options>> = {}) {
